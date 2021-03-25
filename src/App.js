f786d3766196088bf2d45c1d84693b8688c003df
Mauricio Lima <mauricio@codebit.com.br>
2021-03-25 20:41:35 -0300

    feat: add login page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import { ThemeProvider } from "@material-ui/core/styles";
 import { ThemeProvider as StyledThemeProvider } from "styled-components";
 import theme from "./styles/theme";
+import LoginPage from "./pages/login/login.component";
 
 function App() {
   return (
     <StyledThemeProvider theme={theme}>
       <ThemeProvider theme={theme}>
-        <div className="App">
-          <h1>Comarev DashBoard</h1>
-        </div>
+        <LoginPage />
       </ThemeProvider>
     </StyledThemeProvider>
   );
---
+++ src/pages/login/login.component.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+const INITIAL_STATE = {
+  email: false,
+  password: false,
+};
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const SignIn = () => {
+  const classes = useStyles();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [formError, setError] = useState(INITIAL_STATE);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!email || !password) setError({ email: !email, password: !password });
+  };
+
+  useEffect(() => {
+    if (email) setError((currentState) => ({ ...currentState, email: !email }));
+    if (password)
+      setError((currentState) => ({ ...currentState, password: !password }));
+  }, [email, password]);
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Comarev Parceiros
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Endereço de Email"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            error={formError.email}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Senha"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            error={formError.password}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={handleSubmit}
+          >
+            Logar
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Esqueceu sua senha?
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
+};
+
+export default SignIn;
