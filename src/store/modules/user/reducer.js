9957e3e575c3b10f11c5d4be5da78b440a10c614
Mauricio Lima <mauricio@codebit.com.br>
2021-05-06 20:01:39 -0300

    feat: set userReducer initial state

--- src/store/modules/user/reducer.js
+++ src/store/modules/user/reducer.js
@@ -1,6 +1,20 @@
 import { USER_SIGN_IN, USER_SIGN_OUT } from './actions';
 
-const user = (state = {}, action) => {
+const INITIAL_STATE = {
+  id: 0,
+  full_name: '',
+  email: '',
+  cpf: '',
+  address: '',
+  cellphone: '',
+  picture_url: null,
+  admin: false,
+  active: false,
+  created_at: '',
+  updated_at: '',
+};
+
+const user = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case USER_SIGN_IN:
       return action.data;
