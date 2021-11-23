1ea3ce2e8b3c99c5a3b5ae08b73e86a0ff998bba
RenanRSilva <renanrambuls@gmail.com>
2021-11-23 19:16:03 -0300

    chore: move test file to the right folder

--- src/components/companies-list/__tests__/companies-list.test.js
+++ src/components/companies-list/__tests__/companies-list.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import CompaniesList from './companies-list';
-import mockedCompanies from '../../test/fixtures/companies';
+import CompaniesList from '../companies-list';
+import mockedCompanies from '../../../test/fixtures/companies';
 
 describe('Companies List', () => {
-  it('render 3 lines', () => {
+  it('renders 3 lines', () => {
     render(<CompaniesList data={mockedCompanies} />);
 
     mockedCompanies.forEach((company) => {
