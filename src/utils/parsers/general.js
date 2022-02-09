52f8ee2f2ca3229f32ff2e73f3ce892f88e55fc0
Gabriel Belgamo <19699724+belgamo@users.noreply.github.com>
2022-02-08 23:50:51 -0300

    fix: fix amount incosistency when mark invoice as paid (#42)

--- src/utils/parsers/general.js
+++ src/utils/parsers/general.js
@@ -1,4 +1,12 @@
 export const onlyNumbers = (value) => value.replace(/\D/g, '');
 
-export const parseCurrency = (value) =>
-  value.toString().replace('.', '').replace(',', '.').replace('R$', '');
+export const parseCurrency = (value) => {
+  if (typeof value === 'number') return value;
+
+  return value
+    .toString()
+    .replace('.', '')
+    .replace(',', '.')
+    .replace('R$', '')
+    .trim();
+};
