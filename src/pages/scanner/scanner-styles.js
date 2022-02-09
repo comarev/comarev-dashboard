45fe4c2bd87dd1719bd5fc4dd3ee3577c4da6abb
Gabriel Belgamo <19699724+belgamo@users.noreply.github.com>
2022-02-08 22:04:34 -0300

    feat: add small tweaks (#40)

--- src/pages/scanner/scanner-styles.js
+++ src/pages/scanner/scanner-styles.js
@@ -10,8 +10,13 @@ export const Reader = styled.div`
 `;
 
 export const Container = styled.div`
-  display: flex;
   height: 100vh;
+  width: 100%;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  height: 100%;
   width: 100%;
   flex-direction: column;
 
