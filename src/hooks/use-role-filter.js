6a06fca26499db62fad6e09a9b5e06ed8d135ca9
Isaac Alencar <alencarisaac3@gmail.com>
2022-04-25 10:24:03 -0300

    Refactor: add nullish operator
    Add nullish operator to avoid trowing erros on undefined values

--- src/hooks/use-role-filter.js
+++ src/hooks/use-role-filter.js
@@ -3,7 +3,7 @@ import { USER_ROLES } from 'utils/constants';
 
 function useRoleFilter(roles = []) {
   const user = useSelector((state) => state.user);
-  const userRoles = USER_ROLES.filter((role) => user[role]);
+  const userRoles = USER_ROLES.filter((role) => user?.[role]);
 
   const hasPermission = roles.some((role) => userRoles.includes(role));
 
