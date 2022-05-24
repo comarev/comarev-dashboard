f7a39a724aeb77f1fb183c40a84c1dcb74d4366f
Moises <moisesalonso01@gmail.com>
2022-05-24 12:00:41 -0300

    refactor: remove unecessary code from hook

--- src/hooks/use-companies.js
+++ src/hooks/use-companies.js
@@ -3,14 +3,11 @@ import { getCompanies } from 'service/company';
 
 const useCompanies = () => {
   const { data, status } = useQuery('companies', getCompanies);
-
   const companies = data?.data;
 
   const isLoading = ['idle', 'loading'].includes(status);
 
-  const selectedCompany = !isLoading ? companies[0] : null;
   return {
-    selectedCompany,
     companies,
     isLoading,
   };
