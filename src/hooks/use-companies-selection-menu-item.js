400de7d3f48c4adf800187020013ceb9b01afb48
Moises <moisesalonso01@gmail.com>
2022-05-24 13:53:36 -0300

    fix: fix hook selection system

--- src/hooks/use-companies-selection-menu-item.js
+++ src/hooks/use-companies-selection-menu-item.js
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react';
 
 const useCompaniesSelectionMenuItem = (companies) => {
-  const [selectedCompany, setSelectedCompany] = useState(null);
+  const [selectedCompany, setSelectedCompany] = useState('');
 
   useEffect(() => {
     if (companies?.length >= 1) {
-      setSelectedCompany(companies?.[0]);
+      setSelectedCompany(companies?.[0].id);
     }
   }, [companies]);
   return { selectedCompany, setSelectedCompany };
