a775b64bff070dda202c784b6adb8818330c15b5
Moises <moisesalonso01@gmail.com>
2022-05-25 11:08:09 -0300

    refactor: change import from discount request

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,7 +9,7 @@ import CompanyRegister from 'pages/companies/company-register.component';
 import CompanyEdit from 'pages/companies/company-edit.component';
 import UserRegister from 'pages/users/user-register.component';
 import UserEdit from 'pages/users/user-edit.component';
-import DiscountRequests from 'pages/users/user-invoices.component';
+import DiscountRequests from 'pages/users/user-discount-request.component';
 import InvoiceList from 'pages/invoices/invoice-list.component';
 import InvoiceRegister from 'pages/invoices/invoice-register.component';
 import InvoiceView from 'pages/invoices/invoices-view.component';
