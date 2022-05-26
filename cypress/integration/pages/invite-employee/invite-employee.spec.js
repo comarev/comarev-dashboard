bc209bda3da1aca05184f8b88ef827656be8a313
Will Apolinario <willieapolinario@gmail.com>
2022-05-26 15:47:28 -0300

    refactored integration test by nesting describes blocks and made its tests titles more clear

--- cypress/integration/pages/invite-employee/invite-employee.spec.js
+++ cypress/integration/pages/invite-employee/invite-employee.spec.js
@@ -1,33 +1,35 @@
 describe('Invite new employee', () => {
-  const manager = 'manager@example.com';
-  const password = '123456';
+  describe('when the user is a manager', () => {
+    const manager = 'manager@example.com';
+    const password = '123456';
 
-  beforeEach(() => {
-    cy.visit('/');
-    cy.get('#email').type(manager);
-    cy.get('#password').type(password);
-    cy.get('[type=submit]').click();
-    cy.contains('Convidar novo empregado').click();
+    beforeEach(() => {
+      cy.visit('/');
+      cy.get('#email').type(manager);
+      cy.get('#password').type(password);
+      cy.get('[type=submit]').click();
+      cy.contains('Convidar novo empregado').click();
+    });
+
+    it('Invite new employee option must be visible when user is a manager', () => {
+      cy.contains('Convidar novo empregado').should('be.visible');
+    });
+
+    it('should display an e-mail form when visiting invite new employee link', () => {
+      cy.get('#email').should('be.visible');
+      cy.get('button[aria-label="Enviar e-mail"]').should('be.visible');
+    });
+
+    it('should display error message when trying to submit with empty input', () => {
+      cy.get('button[aria-label="Enviar e-mail"]').click();
+      cy.contains('Campo obrigatório').should('be.visible');
+    });
   });
 
-  it('Invite new employee option must be visible when user is a manager', () => {
-    cy.contains('Convidar novo empregado').should('be.visible');
-  });
-
-  it('should display an e-mail form when visiting invite new employee link', () => {
-    cy.get('#email').should('be.visible');
-    cy.get('button[aria-label="Enviar e-mail"]').should('be.visible');
-  });
-
-  it('should display error message when trying to submit with empty input', () => {
-    cy.get('button[aria-label="Enviar e-mail"]').click();
-    cy.contains('Campo obrigatório').should('be.visible');
-  });
-});
-
-describe('Invite new employee option must not exist', () => {
-  it('Invite new employee option must not exist when user is other than a manager', () => {
-    cy.login('admin@example.com', '123456');
-    cy.contains('Convidar novo empregado').should('not.exist');
+  describe('when the user is not a manager', () => {
+    it('Invite new employee option must not exist', () => {
+      cy.login('admin@example.com', '123456');
+      cy.contains('Convidar novo empregado').should('not.exist');
+    });
   });
 });
