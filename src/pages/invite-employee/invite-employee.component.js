8eeb3f9972a654dce66b23ca4df9876836b26758
Will Apolinario <willieapolinario@gmail.com>
2022-05-26 14:19:09 -0300

    changing the button's aria-label dynamically  when form is being submitted

--- src/pages/invite-employee/invite-employee.component.js
+++ src/pages/invite-employee/invite-employee.component.js
@@ -50,7 +50,7 @@ const InviteEmployee = () => {
               variant='contained'
               color='primary'
               disabled={isLoading}
-              aria-label='Enviar e-mail'
+              aria-label={isLoading ? 'Enviando e-mail' : 'Enviar e-mail'}
             >
               {isLoading ? (
                 <CircularProgress data-testid='login-spinner' size={25} />
