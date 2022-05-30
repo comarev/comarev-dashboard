96f11f76135796f7a714c2043e7cbdd7c481ea88
Moises <moisesalonso01@gmail.com>
2022-05-30 17:11:26 -0300

    feat: add test to the table populated

--- src/pages/users/components/___test___/discont-request-table.component.test.js
+++ src/pages/users/components/___test___/discont-request-table.component.test.js
@@ -1,23 +1,29 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import DiscountRequestTable from 'pages/users/components/discont-request-table.component';
 import mockedDiscountRequest from 'test/fixtures/discount-request';
 
 const renderTable = (props) =>
   render(<DiscountRequestTable {...props} data-testid='users-loading' />);
 
-const getTable = () => screen.getByTestId('discount-request-table');
-const getAllRows = () => screen.getAllByRole('list');
+const getDiscountRequestTable = () =>
+  screen.getByTestId('discount-request-table');
+
+const getDiscountRequestTableRow = (discountRequest) =>
+  screen.getByTestId('table-row/' + discountRequest.id);
 
 describe('Discount request table component', () => {
   it('should render', () => {
     renderTable();
-    const table = getTable();
+    const table = getDiscountRequestTable();
     expect(table).toBeInTheDocument();
   });
-  it('should render the invoices row', () => {
-    renderTable({ invoiceList: mockedDiscountRequest });
-    const rows = getAllRows();
-    console.log(rows);
-    expect(rows.length).toBeInTheDocument(mockedDiscountRequest.length);
+
+  it('should render 3 rows in the table', async () => {
+    renderTable({ invoicesList: mockedDiscountRequest });
+    await waitFor(() => {
+      mockedDiscountRequest.forEach((discountRequest) => {
+        expect(getDiscountRequestTableRow(discountRequest)).toBeInTheDocument();
+      });
+    });
   });
 });
