a8173d5a37fd46ef4812b636641a573eccd5c20f
Moises <moisesalonso01@gmail.com>
2022-05-30 17:54:28 -0300

    feat: add test to the rows informations

--- src/pages/users/components/___test___/discont-request-table.component.test.js
+++ src/pages/users/components/___test___/discont-request-table.component.test.js
@@ -1,6 +1,10 @@
 import { render, screen, waitFor } from '@testing-library/react';
-import DiscountRequestTable from 'pages/users/components/discont-request-table.component';
+import DiscountRequestTable, {
+  getAllowedCellLabelColor,
+  getAllowedCellLabel,
+} from 'pages/users/components/discont-request-table.component';
 import mockedDiscountRequest from 'test/fixtures/discount-request';
+import { getDateFormated, getHourFormated } from 'utils/formatters/general';
 
 const renderTable = (props) =>
   render(<DiscountRequestTable {...props} data-testid='users-loading' />);
@@ -10,6 +14,9 @@ const getDiscountRequestTable = () =>
 
 const getDiscountRequestTableRow = (discountRequest) =>
   screen.getByTestId('table-row/' + discountRequest.id);
+
+const getDiscountRequestTableRowChip = (discountRequest) =>
+  screen.getByTestId('table-row/chip/' + discountRequest.id);
 
 describe('Discount request table component', () => {
   it('should render', () => {
@@ -23,6 +30,29 @@ describe('Discount request table component', () => {
     await waitFor(() => {
       mockedDiscountRequest.forEach((discountRequest) => {
         expect(getDiscountRequestTableRow(discountRequest)).toBeInTheDocument();
+      });
+    });
+  });
+
+  it('render the right information on the rows', async () => {
+    renderTable({ invoicesList: mockedDiscountRequest });
+    await waitFor(() => {
+      mockedDiscountRequest.forEach((discountRequest) => {
+        const row = getDiscountRequestTableRow(discountRequest);
+        const chip = getDiscountRequestTableRowChip(discountRequest);
+        expect(row).toBeInTheDocument();
+
+        expect(row).toHaveTextContent(discountRequest.user.full_name);
+        expect(row).toHaveTextContent(discountRequest.user.email);
+        expect(row).toHaveTextContent(
+          getDateFormated(discountRequest.created_at)
+        );
+        expect(row).toHaveTextContent(
+          getHourFormated(discountRequest.created_at)
+        );
+        expect(row).toHaveTextContent(
+          getAllowedCellLabel(discountRequest.allowed)
+        );
       });
     });
   });
