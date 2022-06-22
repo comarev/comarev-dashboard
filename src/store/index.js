f5ead4f989725c368b66d16f4e34eaccd91eb51c
antonio <aq.simei@gmail.com>
2022-06-22 12:49:30 -0300

    feat: set up redux devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,13 @@
-import { createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 import rootReducer from './modules/rootReducer';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer,
+  compose(
+    applyMiddleware(),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : (f) => f
+  )
+  );
 
 export default store;
