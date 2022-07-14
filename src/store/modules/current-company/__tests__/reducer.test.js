d44e50c70bf25635b96844ae4de4f34de7ce47be
antonio <aq.simei@gmail.com>
2022-07-14 16:32:53 -0300

    test: add missing current company reducer tests

--- src/store/modules/current-company/__tests__/reducer.test.js
+++ src/store/modules/current-company/__tests__/reducer.test.js
@@ -13,15 +13,40 @@ describe('Current company reducer', () => {
     });
   });
 
-  describe('when the action is relevant for the reducer', () => {
-    it('returns the updated state', () => {
-      const initialState = { id: 10 };
-      const newState = { id: 20 };
-      const expectedState = { id: 20 };
-      const action = updateCompany(newState);
-      const updatedState = currentCompanyReducer(initialState, action);
+  describe('when the action type is relevant for the reducer', () => {
+    describe('when the action is SET_CURRENT_COMPANY', () => {
+      it("handles 'SET_CURRENT_COMPANY' ", () => {
+        const initialState = { id: 10 };
+        const newState = { id: 20 };
+        const expectedState = { id: 20 };
+        const action = updateCompany(newState);
+        const updatedState = currentCompanyReducer(initialState, action);
 
-      expect(updatedState).toStrictEqual(expectedState);
+        expect(updatedState).toStrictEqual(expectedState);
+      });
     });
-  });
+    describe("when the action type is 'persist/REHYDRATE'", () => {
+      it("handles 'persist/REHYDRATE' ", () => {
+        const initialState = { id: 0 };
+        const newState = { id: 10 };
+        const expectedState = { id: 10 };
+        const action = {type: 'persist/REHYDRATE', payload: { user: { companies: [{ company : newState }] } }};
+        const returnedState = currentCompanyReducer(initialState, action);
+
+        expect(returnedState).toStrictEqual(expectedState);
+      })
+    });
+
+    describe("when the action type is 'USER_SIGN_IN'", () => {
+      it("handles 'USER_SIGN_IN' ", () => {
+        const initialState = { id: 0 };
+        const newState = { id: 10 };
+        const expectedState = { id: 10 };
+        const action = {type: '@user/SIGN_IN', payload: {companies: [{ company : newState }]} };
+        const returnedState = currentCompanyReducer(initialState, action);
+
+        expect(returnedState).toStrictEqual(expectedState);
+      })
+    });
+  })
 });
